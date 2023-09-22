de3aeeafcd610d0e86d8014a24294ac1e1d66c04
Guilherme Furlaneto <gpfurlaneto@gmail.com>
2023-09-22 16:05:33 -0300

    player

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,63 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-import axios from "axios"
+import axios from 'axios';
+import Color from 'color';
+import MidiPlayer from 'react-midi-player';
+import { Link } from 'react-router-dom';
 
-import { useSession } from "@clerk/clerk-react"
+import { useSession } from '@clerk/clerk-react';
+import styled from '@emotion/styled';
 
-import { useMidi } from "../hooks/useMidi"
+import { useMidi } from '../hooks/useMidi';
 
+const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -150px 0 0 -300px;
+  display: flex;
+  flex-direction: column;
+}
+`
+const Input = styled.input`
+  height: 40px;
+  width: 600px;
+  border-radius: 10px;
+  padding: 6px 15px;
+  font-size: 18px;
+  margin-bottom: 20px;
+`
+
+const RowContainer = styled.div`
+  gap: 10px;
+  padding: 5px 10px;
+  display: flex;
+  flex-direction: column;
+`
+
+const Row = styled.div`
+  padding: 5px 10px;
+  width: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: ${({ theme }) => Color(theme.backgroundColor).darken(0.2).hex()};
+  &:hover {
+    background: ${({ theme }) => theme.secondaryBackgroundColor};
+  }
+`
+interface FileDesc {
+  name: string
+  file: File
+  buffer: string
+}
 const baseURL = process.env.API_URL
 export function Search() {
   const { session } = useSession()
-  const { setMidiFile } = useMidi()
+  const { midiFile, setMidiFile } = useMidi()
   const [searchValue, setSearchValue] = useState("")
+  const [arrayBuffers, setArrayBuffers] = useState<FileDesc[]>([])
 
   const doRequest = async () => {
     const response = await axios.get(`${baseURL}/search?query=${searchValue}`, {
@@ -22,16 +69,27 @@ export function Search() {
 
     const blob = new Blob([response.data], { type: "audio/midi" })
     const file = new File([blob], "MIDI_sample.mid", { type: "audio/midi" })
-    setMidiFile(file)
-    console.log({ file })
+
+    try {
+      // Convert the Blob to a Base64 string
+      const reader = new FileReader()
+      reader.onload = function () {
+        if (reader.result) {
+          const base64String = (reader.result as any).split(",")[1]
+          // Use the base64String as needed
+          setArrayBuffers([{ name: file.name, file, buffer: base64String }])
+        }
+      }
+      reader.readAsDataURL(blob)
+    } catch (error) {
+      console.error("An error occurred:", error)
+    }
   }
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      <input
-        className="w-[600px] rounded-lg p-3 text-black"
+    <Content>
+      <Input
+        placeholder="Search for a song"
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
         onKeyDown={(e) => {
@@ -40,6 +98,19 @@ export function Search() {
           }
         }}
       />
-    </main>
+      {arrayBuffers.length > 0 && (
+        <RowContainer>
+          {arrayBuffers.map((fileDesc, index) => {
+            return (
+              <Row onClick={() => setMidiFile(fileDesc.file)}>
+                <Link to="/playground">{fileDesc.name}</Link>
+                {/* {fileDesc.buffer} */}
+                <MidiPlayer key="1" data={atob(fileDesc.buffer)} />
+              </Row>
+            )
+          })}
+        </RowContainer>
+      )}
+    </Content>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,92 @@
+import { CSSProperties } from 'react';
+
+import Color from 'color';
 import { Link } from 'react-router-dom';
 
 import { UserButton } from '@clerk/clerk-react';
+import styled from '@emotion/styled';
 
 import { Search } from '../components/Search';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: ${({ theme }) => Color(theme.backgroundColor).darken(0.2).hex()};
+  height: 3rem;
+  flex-shrink: 0;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: calc(100vh - 3rem);
+  position: relative;
+`
+
+export const Tab = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center; 
+  padding: 0.5rem 1rem;
+  font-size: 0.75rem;
+  border-top: solid 0.1rem transparent;
+  color: ${({ theme }) => theme.secondaryTextColor};
+
+  &.active {
+    color: ${({ theme }) => theme.textColor};
+    background: ${({ theme }) => theme.backgroundColor};
+    border-top-color: ${({ theme }) => theme.themeColor};
+  }
+
+  &:hover {
+    background: ${({ theme }) => theme.highlightColor};
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+}
+`
+
+export const TabTitle = styled.span`
+  margin-left: 0.5rem;
+
+  @media (max-width: 850px) {
+    display: none;
+  }
+`
+
+const FlexibleSpacer = styled.div`
+  flex-grow: 1;
+`
+
+export const IconStyle: CSSProperties = {
+  width: "1.3rem",
+  height: "1.3rem",
+  fill: "currentColor",
+}
 
 export function HomePage() {
   return (
     <div>
-      <UserButton />
-      <h1>Howdy sir!</h1>
-      <Search />
-      <Link to="/playground">Check this out!</Link>
+      <Container>
+        <FlexibleSpacer />
+        <Tab className="flex flex-row gap-10">
+          <Link to="https://ae.studio" target="_blank">
+            <TabTitle>Made with ❤️ by AE Studio</TabTitle>
+          </Link>
+        </Tab>
+        <Tab className="flex flex-row gap-10">
+          <UserButton />
+        </Tab>
+      </Container>
+      <Content>
+        <Content>
+          <Search />
+        </Content>
+      </Content>
     </div>
   )
 }
