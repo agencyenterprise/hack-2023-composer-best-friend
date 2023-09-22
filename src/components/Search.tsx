ec8487ed82c8bdd63d2c6bd7d29cc1457d6d85df
Guilherme Furlaneto <gpfurlaneto@gmail.com>
2023-09-22 08:44:23 -0300

    Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import axios from 'axios';
+
+export function Search() {
+  const [searchValue, setSearchValue] = useState("")
+  const doRequest = async () => {
+    const midi = await axios.get(
+      `http://localhost:4000/search?query=${searchValue}`,
+    )
+    console.log(midi)
+  }
+
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
+      <input
+        className="w-[600px] rounded-lg p-3 text-black"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            doRequest()
+          }
+        }}
+      />
+    </main>
+  )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+
+import { Search } from '../components/Search';
 
 export function HomePage() {
   return (
     <div>
       <h1>Howdy sir!</h1>
+      <Search />
       <Link to="/playground">Check this out!</Link>
     </div>
   )
